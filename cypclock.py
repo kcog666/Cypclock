8b57b039cb3fcfd4e75ff13a3b27beae9dafec75
cypressru <140924725+cypressru@users.noreply.github.com>
2023-10-10 22:48:41 -0500

    Update cypclock.py
    Added Korn AMV button with blue text on a button

--- cypclock.py
+++ cypclock.py
@@ -1,11 +1,30 @@
 from tkinter import *
 from time import strftime
+import webbrowser
 
 root = Tk()
 root.geometry("420x420")
 root.resizable(0,0)
 root.title('Cypclock 0.1')
 
+new = 1
+url = "https://www.youtube.com/watch?v=Q44DA9-TH2o&list=PLLGIaswzK8nerfBww5vX312Bt2B5A24om"
+
+
+def openweb():
+    webbrowser.open(url,new=new)
+def expand ():
+    pass
+
+
+# button code
+my_button = Button(root,
+                   text="Korn AMVs",
+                   command=openweb,
+                   font=("calibri", 24),
+                   fg="blue")
+my_button.pack(pady=100)
+# clock code
 Label(root,text = 'test by Cyp', font = 'arial 20 bold').pack(side=BOTTOM)
 
 def time():
